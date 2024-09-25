c1556ec74097c0d2904ef25af377974c1d33c4c4
Piotr Markiewicz <mr.piotr.markiewicz@gmail.com>
2024-09-25 23:35:04 +0200

    GetMinTempAndMaxWindByCountry

---
+++ apps/WeatherVisualizer/Backend/WeatherVisualizer/WeatherVisualizer.Infrastructure/Migrations/20240925213208_MinTempAndMaxSpeedStoredProcedure.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherVisualizer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MinTempAndMaxSpeedStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                CREATE PROCEDURE GetMinTempAndMaxWindByCountry
+                    @CountryName NVARCHAR(100),
+                    @TemperatureThreshold FLOAT
+                AS
+                BEGIN
+                    -- Select the minimum temperature and maximum wind speed for a given country 
+                    -- where the minimum temperature is less than the specified threshold
+                    SELECT 
+                        MIN(TemperatureC) AS MinTemperature,
+                        MAX(WindKph) AS MaxWindSpeed
+                    FROM 
+                        WeatherMeasurementEntries
+                    WHERE 
+                        Country = @CountryName
+                        AND TemperatureC < @TemperatureThreshold;
+                END;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP  PROCEDURE IF EXISTS dbo.GetMinTempAndMaxWindByCountry");
+        }
+    }
+}
