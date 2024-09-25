bcb8246492f18ee735ee0fcfba26d0f01c08bfba
Piotr Markiewicz <mr.piotr.markiewicz@gmail.com>
2024-09-25 23:31:28 +0200

    GetMaxWindSpeedByCountry

---
+++ apps/WeatherVisualizer/Backend/WeatherVisualizer/WeatherVisualizer.Infrastructure/Migrations/20240925212814_MaxWindSpeedStoredProcedure.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherVisualizer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MaxWindSpeedStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                CREATE PROCEDURE GetMaxWindSpeedByCountry
+                    @CountryName NVARCHAR(100)
+                AS
+                BEGIN
+                    -- Select the maximum wind speed and the country
+                    SELECT TOP 1
+                        MAX(WindKph) AS MaxWindSpeed,
+                        Country
+                    FROM
+                        WeatherMeasurementEntries
+                    WHERE
+                        Country = @CountryName
+                    GROUP BY
+                        Country;
+                END;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP  PROCEDURE IF EXISTS dbo.GetMaxWindSpeedByCountry");
+        }
+    }
+}
