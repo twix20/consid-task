1297c0c3eddbdccd490145af512ac11ae3549dd9
Piotr Markiewicz <mr.piotr.markiewicz@gmail.com>
2024-09-26 19:19:03 +0200

    rm unused

--- apps/WeatherVisualizer/Frontend/components/max-wind-speed-graph.tsx
+++ apps/WeatherVisualizer/Frontend/components/max-wind-speed-graph.tsx
@@ -11,16 +11,8 @@ import {
   Line,
   Legend,
 } from "recharts";
-import { getCityColor, getRandomColor } from "./utils";
+import { getCityColor } from "./utils";
 import { useMesurmentsToChartData } from "@/hooks/use-mesurments-to-chart-data";
-
-// Mocked data for Max Wind Speed Graph
-const maxWindSpeedData = [
-  { lastUpdate: "2024-09-26 08:00 AM", Chicago: 18, London: 17, Miami: 15 },
-  { lastUpdate: "2024-09-26 09:00 AM", Chicago: 20, London: 18, Miami: 16 },
-  { lastUpdate: "2024-09-26 10:00 AM", Chicago: 22, London: 19, Miami: 17 },
-  { lastUpdate: "2024-09-26 11:00 AM", Chicago: 25, London: 20, Miami: 18 },
-];
 
 export const MaxWindSpeedGraph = () => {
   const { data } = useGetWeatherMeasurements({
