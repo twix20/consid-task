3e9dbd587b63b0ebabc73fa772b7b7c741524361
Piotr Markiewicz <mr.piotr.markiewicz@gmail.com>
2024-09-26 18:31:31 +0200

    rm console log

--- apps/WeatherVisualizer/Frontend/components/dashboard-02.tsx
+++ apps/WeatherVisualizer/Frontend/components/dashboard-02.tsx
@@ -17,8 +17,6 @@ import { MinTemperatureGraph } from "./min-temperature-graph";
 import considLogoUrl from "./consid-logo.png";
 
 export default function Dashboard() {
-  console.log(considLogoUrl);
-
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
